e4b61171bdd99c6d31695f8948768c659a894af8
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-11-29 21:05:09 +0200

    added tailwind setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ export default function Home() {
     return (
         <div>
             <h1>Home Page!</h1>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
