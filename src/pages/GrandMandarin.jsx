83018ca7f26a146e4645fe7ebcd679a68272ce4f
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-11-29 20:57:37 +0200

    installed react router dom and added initial setup for pages and components

---
+++ src/pages/GrandMandarin.jsx
@@ -0,0 +1,7 @@
+export default function GrandMandarin() {
+    return (
+        <div>
+            <h1>Grand Mandarin Page!</h1>
+        </div>
+    )
+}
---
+++ src/pages/RoyalPrincess.jsx
@@ -0,0 +1,7 @@
+export default function RoyalPrincess() {
+    return (
+        <div>
+            <h1>Royal Princess Page!</h1>
+        </div>
+    )
+}
