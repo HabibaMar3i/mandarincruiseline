39a6984353f6f9dfde55ea23c5131ff353353f17
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-11-29 21:03:18 +0200

    added routing and not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactsUs';
+import NotFound from './pages/NotFound';
+import RoyalPrincess from './pages/RoyalPrincess';
+import GrandMandarin from './pages/GrandMandarin';
 
 function App() {
+  const routes = createBrowserRouter([
+    {path: '/', element: <Layout/>, children:[
+      {index: true, element: <Home/>},
+      {path: '/about-us', element: <AboutUs/>},
+      {path: '/contact-us', element: <ContactUs/>},
+      {path: '/royal-princess', element: <RoyalPrincess/>},
+      {path: '/grand-mandarin', element: <GrandMandarin/>},
+      {path: '*', element: <NotFound/>}
+    ]}
+  ])
   return (
     <>
-      <h1>Test app!</h1>
+      <RouterProvider router={routes} />
     </>
   )
 }
