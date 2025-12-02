efdd2efddf1a3ca5dd493fd6d4d963b1a5771ea5
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-12-02 21:45:03 +0200

    added scroll to top functionality

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function Layout() {
     return (
         <div className="bg-[#F2ECE7] min-h-screen">
+            <ScrollToTop />
             <Navbar/>
             <Outlet />
             <Footer/>
